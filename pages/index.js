868e9215ca744da189e0f6d76280832713f606ae
Tino Reyna <tinoreyna1984@gmail.com>
2023-03-06 15:55:00 -0500

    skeleton on index loading

--- pages/index.js
+++ pages/index.js
@@ -43,6 +43,21 @@ export default function Home() {
           (<div className='flex h-screen w-full justify-center items-center'>
             <div className='flex flex-col items-start my-3 mx-3 md:mx-4 lg:mx-5'>
               <h1 className='text-2xl font-black drop-shadow-xl text-center'>Loading...</h1>
+              <div className='flex flex-col items-end my-6 mx-8 animate-pulse'>
+                <div className='text-start italic border-r-4'>
+                  <div className="h-6 w-[300px] md:w-[450px] lg:w-[600px] rounded-md bg-gray-300 "></div>
+                </div>
+                <div className="flex flex-col items-end gap-2 my-4 py-3">
+                  <div className="h-4 w-[200px] rounded-md bg-gray-300 "></div>
+                  <div className="h-4 w-[200px] rounded-md bg-gray-300 "></div>
+                </div>
+              </div>
+              <button
+                className='border-solid rounded-md bg-gray-300 w-[200px]
+                py-3 px-5 animate-pulse' type='button'
+              >
+                <div className="h-6 w-full rounded-md bg-gray-300 "></div>
+              </button>
             </div>
           </div>) :
           (<div className='flex h-screen w-full justify-center items-center'>
